21626b32dbf343e796c3ac4fb8ca82227d3632fb
Han Ngo <bahanngo7@gmail.com>
2026-02-09 17:39:43 +0700

    kakak

--- _data/posts.js
+++ _data/posts.js
@@ -1,19 +1,3 @@
-// module.exports = async function () {
-//     console.log("Fetching posts from WordPress API...");
-//     try {
-//         // Node 18+ có sẵn fetch, không cần import node-fetch
-//         const response = await fetch('http://headless-project.py-media.com/wp-json/wp/v2/posts?_embed&per_page=12');
-//         if (!response.ok) {
-//             throw new Error(`Failed to fetch: ${response.status}`);
-//         }
-//         const posts = await response.json();
-//         return posts;
-//     } catch (error) {
-//         console.error("Error fetching posts:", error);
-//         return [];
-//     }
-// };
-
 module.exports = async function () {
     const query = `{
         posts {
